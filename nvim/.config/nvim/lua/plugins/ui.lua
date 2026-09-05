85a9a97c5a87609cf753804f6bb92f5dcb00a034
TienDVD <doanvanduytien@gmail.com>
2026-09-05 12:35:06 +0700

    feat: update config

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,41 +1,41 @@
 return {
   -- Use neo-tree instead snacks explorer
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false,
-
-    init = function()
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-    end,
-
-    config = function()
-      require("neo-tree").setup({
-        source_selector = {
-          winbar = true,
-          sources = {
-            { source = "filesystem", display_name = "Files" },
-            { source = "git_status", display_name = "Git" },
-          },
-          truncation_character = "...",
-        },
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-            hide_gitignored = false,
-          },
-        },
-      })
-      vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree", silent = true })
-    end,
-  },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   lazy = false,
+  --
+  --   init = function()
+  --     vim.g.loaded_netrw = 1
+  --     vim.g.loaded_netrwPlugin = 1
+  --   end,
+  --
+  --   config = function()
+  --     require("neo-tree").setup({
+  --       source_selector = {
+  --         winbar = true,
+  --         sources = {
+  --           { source = "filesystem", display_name = "Files" },
+  --           { source = "git_status", display_name = "Git" },
+  --         },
+  --         truncation_character = "...",
+  --       },
+  --       filesystem = {
+  --         filtered_items = {
+  --           visible = true,
+  --           hide_dotfiles = false,
+  --           hide_gitignored = false,
+  --         },
+  --       },
+  --     })
+  --     vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree", silent = true })
+  --   end,
+  -- },
   -- Enable dashboard (no header), disable explorer
   {
     "folke/snacks.nvim",
@@ -53,30 +53,44 @@ return {
         ]],
         },
       },
-      explorer = { enabled = false },
-    },
-    keys = {
-      { "<leader>e", false },
-      { "<leader>E", false },
+      explorer = {
+        hidden = true,
+        ignored = true,
+      },
     },
   },
-
   -- Config transparent tokyo night
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
   {
-    "folke/tokyonight.nvim",
+    "folke/noice.nvim",
     opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+      cmdline = {
+        enabled = false,
       },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato",
     },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-moon",
+      colorscheme = "catppuccin-nvim",
     },
   },
 }
