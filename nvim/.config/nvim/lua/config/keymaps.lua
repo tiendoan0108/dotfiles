fa13ca5cdb83b1281c8c260ba7428b471ca0c72b
TienDVD <doanvanduytien@gmail.com>
2026-09-16 21:58:29 +0700

    update config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,7 +5,25 @@
 
 -- Absolute path:line:col (e.g. /home/user/project/lua/plugins/init.lua:42:10)
 vim.keymap.set({ "n", "v" }, "<leader>cP", function()
-  local result = string.format("%s:%d:%d", vim.fn.expand("%:p"), vim.fn.line("."), vim.fn.col("."))
+  local path = vim.fn.expand("%:p")
+  local mode = vim.fn.mode()
+  local result
+
+  if mode == "V" then
+    -- Visual line mode: copy path:startline-endline
+    local start_line = vim.fn.line("v")
+    local end_line = vim.fn.line(".")
+    if start_line > end_line then
+      start_line, end_line = end_line, start_line
+    end
+    result = string.format("%s:%d-%d", path, start_line, end_line)
+  else
+    -- Normal mode (or other visual modes): copy path:line:col
+    result = string.format("%s:%d:%d", path, vim.fn.line("."), vim.fn.col("."))
+  end
+
   vim.fn.setreg("+", result)
   print("Copied: " .. result)
-end, { desc = "Copy absolute path:line:col" })
+end, { desc = "Copy absolute path:line:col (or line range in V-mode)" })
+
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Thoát chế độ insert bằng jk" })
